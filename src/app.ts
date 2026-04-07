ced3182f474d71fa3a18ee8b504875308b69f22b
Galand Noah <galandnoah@gmail.com>
2026-04-07 14:12:26 +0100

    Setting up the app

---
+++ src/app.ts
@@ -0,0 +1,75 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from'cors';
+import logger from './config/logger';
+
+
+
+dotenv.config()
+
+export const app = express()
+const PORT = parseInt(process.env.PORT!) || 5000
+
+/**
+ * SECURITY MIDDLEWARE
+ */
+app.use(cors(
+  {
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }
+))
+
+// I will add helmet  later
+
+/**
+ * GENERAL MIDDLEWARE
+ */
+// compression middleware
+// cookieParser middleware
+// morgan middleware
+// urlencoded middleware
+// json serializer middleware
+app.use(express.json({
+  limit:"10mb"
+}));
+
+
+/**
+ * Global Rate Limiter middleware
+ */
+
+
+
+/**
+ * API ROUTES
+ */
+
+
+
+/**
+ * ERROR HANDLING
+ */
+//notFound
+//errorHandler
+
+/**
+ * START SERVER
+ */
+const start = async ()=>{
+  const server = app.listen(PORT, ()=>{
+     logger.info(`SERVER RUNNING ON PORT ${PORT}`)
+  })
+
+  //CRON JOBS
+
+  //Graceful shutdown
+}
+
+start()
+  .catch(err =>{
+    process.exit(1)
+  })
+
+export default app;
